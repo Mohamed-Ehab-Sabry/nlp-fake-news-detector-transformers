8db98ddee568f70331cd3da0b71b0c08b15da09f
merehan <merehan.ibraheem@gmail.com>
2026-04-13 11:37:32 +0200

    feat(data): load dataset and create train/val/test split

---
+++ src/features/dl_text_preprocessing.py
@@ -0,0 +1,29 @@
+# import libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# load cleaned dataset
+def load_clean_data(filepath):
+    df = pd.read_csv(filepath)
+    return df
+
+# Split data 
+def split_data(df):
+    X = df["cleaned_text"]
+    y = df["target"]
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_val,
+        y_train_val,
+        test_size= 0.15/0.8,
+        random_state= 42,
+        stratify=y_train_val
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
